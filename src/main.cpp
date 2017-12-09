549a0b994daf425f23411c515519051ae74e18f5
Martin Gruscher <martingruscher@gmail.com>
2017-12-09 08:34:59 +0000

    unbind the buffer targets

--- src/main.cpp
+++ src/main.cpp
@@ -140,6 +140,11 @@ int initShaders()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * numIndices , 
 		vIndices, GL_STATIC_DRAW);
+	
+	// cleanly unbind all the buffer targets
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
 }
 
 int main(int argc, char** args) 
