8d9b8f99d6cd333ee723d98077e6a5649037cda8
Martin Gruscher <martingruscher@gmail.com>
2017-12-09 21:01:51 +0000

    add asset folder; remove warnings

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,27 @@ GLubyte vColors[] = {
 	40, 200, 0
 };
 
+GLfloat vTexCoords[] = {
+	0.0, 0.0, 
+	0.0, 1.0, 
+	1.0, 1.0, 
+	1.0, 0.0
+};
+
 GLushort vIndices[] = {
 	0, 1, 2, 
 	2, 3, 0
 };
 
-GLuint buffers[3];
+GLuint buffers[4];
 unsigned short numVertices = 12;
 unsigned short numIndices = 6;
 unsigned short numColors = 12;
+unsigned short numTexCoords = 8;
 
+GLuint textures[3];
 
-GLuint loadShader(const char* shaderSrc, GLenum type) 
+GLuint loadShader(const GLbyte* shaderSrc, GLenum type) 
 {
 	GLuint shader;
 	GLint compiled;
@@ -41,7 +50,7 @@ GLuint loadShader(const char* shaderSrc, GLenum type)
 		return 1;
 	}
 
-	glShaderSource(shader, 1, &shaderSrc, NULL);
+	glShaderSource(shader, 1, reinterpret_cast<const GLchar**>(&shaderSrc), NULL);
 	glCompileShader(shader);
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
 	if (!compiled)
@@ -50,7 +59,7 @@ GLuint loadShader(const char* shaderSrc, GLenum type)
 		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
 		if (infoLen > 1)
 		{
-			char* infoLog = malloc(sizeof(char) * infoLen);
+			char* infoLog = (char*) malloc(sizeof(char) * infoLen);
 
 			glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
 			SDL_Log("shader compile error: %s", infoLog);
@@ -64,31 +73,37 @@ GLuint loadShader(const char* shaderSrc, GLenum type)
 
 }
 
-int initShaders() 
+int initScene() 
 {
 	GLbyte vShaderStr[] = 
-		"attribute vec4 vPosition;\n"
-		"attribute vec3 color;		\n"
-		"varying vec3 col;				\n"
+		"attribute vec4 vPosition;	\n"
+		"attribute vec3 a_color;		\n"
+		"attribute vec2 a_texCoord;	\n"
+		"varying vec2 v_texCoord; 	\n"
+		"varying vec3 col;					\n"
 		"void main()\n"
 		"{\n"
-		"	gl_Position = vPosition;\n"
-		" col = color;						\n"
+		"	gl_Position = vPosition;	\n"
+		" col = a_color;						\n"
+		" v_texCoord = a_texCoord;	\n"
 		"}\n";
 
 	GLbyte fShaderStr[] = 
-		"precision mediump float;			\n"
-		"varying vec3 col;						\n"
-		"void main() 									\n"
-		"{														\n"
-		"	gl_FragColor = vec4(col, 1.0);\n"
-		"}														\n";
+		"precision mediump float;				\n"
+		"varying vec3 col;							\n"
+		"varying vec2 v_texCoord;				\n"
+		"uniform sampler2D s_texture;		\n"
+		"void main() 										\n"
+		"{															\n"
+		"	vec4 v_bgr = texture2D(s_texture, v_texCoord); \n"
+		" gl_FragColor = v_bgr.zyxw; 		\n"
+		"}															\n";
 
 	GLuint vertexShader, fragmentShader, programObject;
 	GLint linked;
 
-	vertexShader = loadShader(vShaderStr, GL_VERTEX_SHADER);
-	fragmentShader = loadShader(fShaderStr, GL_FRAGMENT_SHADER);
+	vertexShader = loadShader(reinterpret_cast<const GLbyte*>(vShaderStr), GL_VERTEX_SHADER);
+	fragmentShader = loadShader(reinterpret_cast<const GLbyte*>(fShaderStr), GL_FRAGMENT_SHADER);
 	programObject = glCreateProgram();
 	if (programObject == 0) 
 	{
@@ -100,6 +115,8 @@ int initShaders()
 	glAttachShader(programObject, fragmentShader);
 	
 	glBindAttribLocation(programObject, 0, "vPosition");
+	glBindAttribLocation(programObject, 1, "a_color");
+	glBindAttribLocation(programObject, 2, "a_texCoord");
 	glLinkProgram(programObject);
 
 	glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
@@ -123,20 +140,27 @@ int initShaders()
 	glUseProgram(programObject);
 
 	GLint posIndex = glGetAttribLocation(programObject, "vPosition");
-	GLint colIndex = glGetAttribLocation(programObject, "color");
-	SDL_Log("posIndex: %d colIndex: %d", posIndex, colIndex);
+	GLint colIndex = glGetAttribLocation(programObject, "a_color");
+	GLint texCoordIndex = glGetAttribLocation(programObject, "a_texCoord");
+	SDL_Log("posIndex: %d colIndex: %d texCoordIndex: %d", 
+			posIndex, colIndex, texCoordIndex);
 
 	// init vertexbuffers
-	glGenBuffers(3, buffers);
+	glGenBuffers(4, buffers);
 	SDL_Log("vb  : %d", buffers[0]);
 	SDL_Log("ib  : %d", buffers[1]);
 	SDL_Log("colb: %d", buffers[2]);
+	SDL_Log("texb: %d", buffers[3]);
 
+	// Buffers for position, color and texture coords
 	glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * numVertices, vVertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, buffers[2]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(GLubyte) * numColors, vColors, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, buffers[3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * numTexCoords, vTexCoords, GL_STATIC_DRAW);
 
+	// Indexbuffer setup
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * numIndices , 
 		vIndices, GL_STATIC_DRAW);
@@ -144,6 +168,77 @@ int initShaders()
 	// cleanly unbind all the buffer targets
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// setup textures
+	// First, we generate the textures we will need
+	glGenTextures(3, textures);
+	SDL_Log("texture[0] %u", textures[0]);
+	// Now, lets work on our first texture, 
+	// which will be the procedural one.
+	// First, we set the currently active 
+	// texture unit. 
+	// Subsequent texture bind will assiciate the
+	// bound texture to the active unit.
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+	
+	// lets create a blue texture
+	GLushort texW = 2;
+	GLushort texH = 2;
+	GLuint sizeTexture = texW * texH * 9;
+	GLubyte proceduralPixels[sizeTexture];
+	int counter=0;
+	for(int i = 0; i < sizeTexture; i+=3) 
+	{
+			proceduralPixels[i]	= 255;
+			proceduralPixels[i+1]	= 0;
+			proceduralPixels[i+2]	= 0;
+
+			SDL_Log("i: %d val: %u",counter,  proceduralPixels[i]);
+			SDL_Log("i+1: %d val: %u", counter+1, proceduralPixels[i+1]);
+			SDL_Log("i+2: %d val: %u", counter+2, proceduralPixels[i+2]);
+	} 
+	
+	SDL_Surface* texSurface = SDL_LoadBMP("../assets/image_one.bmp");
+	if (!texSurface) 
+	{
+		SDL_Log("error surface %s", SDL_GetError());
+		return 1;
+	}
+
+		glTexImage2D(GL_TEXTURE_2D, 0,
+								GL_RGB,
+								texSurface->w, 
+								texSurface->h, 
+								0, // ignore border
+								GL_RGB,
+								GL_UNSIGNED_BYTE,
+								texSurface->pixels);
+		
+		/*glTexImage2D(GL_TEXTURE_2D,
+								0, GL_RGB,
+								texW, texH, 
+								0, 
+								GL_RGB,
+								GL_UNSIGNED_BYTE,
+								proceduralPixels);*/
+	
+
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	GLint textureIndex = glGetUniformLocation(programObject, "s_texture");
+	SDL_Log("textureIndex %d", textureIndex);
+	// Now we tell the sampler in our fragment shader to use 
+	// texture unit 0. 
+	// At runtime, the sampler will take the image data which is
+	// stored in the texture at unit 0. 
+	// This seems a bit weired and complicated, but oh well... :) 
+	glUniform1i(textureIndex, 0);
 	
 }
 
@@ -196,7 +291,7 @@ int main(int argc, char** args)
 		return 1;
 	}
 
-	initShaders();
+	initScene();
 		
 	glClearColor(0.2,0.2,0.2,1);
 
@@ -233,6 +328,11 @@ int main(int argc, char** args)
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, buffers[2]);
 		glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, GL_TRUE, 0, (const void*) 0);
+
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, buffers[3]);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, (const void*) 0);
+
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
 		glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
