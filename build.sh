4911873c0b0f0694ef5bcf213f859e785b997a08
Martin Gruscher <martingruscher@gmail.com>
2017-12-10 09:05:28 +0100

    change to selective building in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 echo "Compiling and linking"
 
-g++ ../src/main.cpp -o game `sdl2-config --cflags --libs` -L/opt/vc/lib/ -lbrcmGLESv2 -I/opt/vc/include -fpermissive
+echo ${HOST_X86}
+
+if [[ -z "${HOST_X86}" ]]; then
+  GLES_LIB="GLESv2"
+else
+	# This is the ARM case 
+  GLES_LIB="brcmGLESv2"
+fi
+
+echo "GLES_LIB" $GLES_LIB
+
+
+g++ ../src/main.cpp -o game `sdl2-config --cflags --libs` -L/opt/vc/lib/ -l$GLES_LIB -I/opt/vc/include -fpermissive
 
 
